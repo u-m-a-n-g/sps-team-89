25afc0ee43f48854b00bac9942fb6908b905fcb3
Umang Malik <umang99m@gmail.com>
2020-08-22 13:54:27 +0530

    Add basic form to get query

--- src/static/script.js
+++ src/static/script.js
@@ -1,7 +1,20 @@
 'use strict';
 
-window.addEventListener('load', function () {
+function plot_graph() {
+  const query = document.getElementById("query-input").value;
 
-  console.log("Hello World!");
+  var data = new FormData();
+  data.append("query",query);
 
-});
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+      document.getElementById("graph-div").innerHTML = this.response;
+      // TODO: INSERT GRAPH.JS GRAPH USING this.response as the data.
+    }
+  });
+
+  xhr.open("POST", "/graph-data");
+  xhr.send(data);
+  return true;
+}
