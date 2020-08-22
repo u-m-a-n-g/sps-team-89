fdae7a4c2d3058903b3a3cc1264242e173de2858
monika400 <35842619+monika400@users.noreply.github.com>
2020-08-22 04:36:40 -0700

    Update script.js

--- src/static/script.js
+++ src/static/script.js
@@ -11,8 +11,35 @@ function plot_graph() {
     if(this.readyState === 4) {
       document.getElementById("graph-div").innerHTML = this.response;
       // TODO: INSERT GRAPH.JS GRAPH USING this.response as the data.
-    }
-  });
+      var myChart = document.getElementById("myChart").getContext("2d");
+         var chart = new Chart(myChart, {
+          type: "line",
+          data: {
+          labels: states,
+          datasets: [
+            {
+              label: "Confirmed Cases",
+              data: confirmed,
+              backgroundColor: "#f1c40f",
+          
+            },
+            {
+              label: "Recovered",
+              data: recovered,
+              backgroundColor: "#2ecc71",
+            
+            },
+            {
+              label: "Deceased",
+              data: deaths,
+              backgroundColor: "#e74c3c",
+          
+            },
+          ],
+       },
+        option: {},
+     });
+    
 
   xhr.open("POST", "/graph-data");
   xhr.send(data);
